b7aad0d9f09972572805ca03496b97209603f19f
Ilya Mordvinkin <ilya_mordvinkin@mail.ru>
2026-08-22 16:51:04 +0400

    fixes

--- src/Dessins/Utils/Polygon.hs
+++ src/Dessins/Utils/Polygon.hs
@@ -2,13 +2,14 @@ module Dessins.Utils.Polygon (axis, xAxis, yAxis, polygon) where
 
 import           Diagrams.Prelude hiding (polygon)
 
+axis :: Floating a => a -> (a -> a) -> a -> a -> a
 axis x fn vertices phi = fn (x * 2 * pi / vertices + phi)
 
 xAxis :: Floating a => a -> a -> a -> a
-xAxis x vertices phi = axis x sin vertices phi
+xAxis x vertices = axis x sin vertices
 
 yAxis :: Floating a => a -> a -> a -> a
-yAxis x vertices phi = axis x cos vertices phi
+yAxis x vertices = axis x cos vertices
 
 polygon :: (Enum p, Floating p) => p -> p -> [P2 p]
 polygon vertices phi = [f x | x <- [1 .. vertices]]
