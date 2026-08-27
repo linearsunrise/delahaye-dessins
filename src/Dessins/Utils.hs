da9b913980802d8a4b3559a763a219883f7bce18
Ilya Mordvinkin <ilya_mordvinkin@mail.ru>
2026-08-28 03:45:50 +0400

    format

--- src/Dessins/Utils.hs
+++ src/Dessins/Utils.hs
@@ -22,50 +22,31 @@ import Dessins.Utils.Polygon
 import Dessins.Utils.Scene
 
 data Matrix2x2 a = Matrix2x2
-  { a11 :: a
-  , a12 :: a
-  , a21 :: a
-  , a22 :: a
+  { a11 :: a, a12 :: a
+  , a21 :: a, a22 :: a
   }
   deriving (Eq, Show)
-
+  
 data Matrix3x3 a = Matrix3x3
-  { b11 :: a
-  , b12 :: a
-  , b13 :: a
-  , b21 :: a
-  , b22 :: a
-  , b23 :: a
-  , b31 :: a
-  , b32 :: a
-  , b33 :: a
+  { b11 :: a, b12 :: a, b13 :: a
+  , b21 :: a, b22 :: a, b23 :: a
+  , b31 :: a, b32 :: a, b33 :: a
   }
   deriving (Eq, Show)
 
-applyMatrixToPoint :: (Num b) => Matrix2x2 b -> (b, b) -> (b, b)
+
+applyMatrixToPoint :: Num b => Matrix2x2 b -> (b, b) -> (b, b)
 applyMatrixToPoint
-  ( Matrix2x2
-      m11
-      m12
-      m21
-      m22
-    )
+  (Matrix2x2 m11 m12
+             m21 m22)
   (x, y) =
     (m11 * x + m12 * y, m21 * x + m22 * y)
 
-applyMatrix3x3ToPoint :: (Num b) => Matrix3x3 b -> (b, b) -> (b, b)
+applyMatrix3x3ToPoint :: Num b => Matrix3x3 b -> (b, b) -> (b, b)
 applyMatrix3x3ToPoint
-  ( Matrix3x3
-      m11
-      m12
-      m13
-      m21
-      m22
-      m23
-      _
-      _
-      _
-    )
+  (Matrix3x3 m11 m12 m13
+             m21 m22 m23
+             _   _   _  )
   (x, y) =
     (m11 * x + m12 * y + m13, m21 * x + m22 * y + m23)
 
@@ -73,10 +54,8 @@ rotateBy :: (Floating a) => a -> (a, a) -> (a, a)
 rotateBy theta =
   applyMatrixToPoint
     ( Matrix2x2
-        { a11 = cos theta
-        , a12 = -sin theta
-        , a21 = sin theta
-        , a22 = cos theta
+        { a11 = cos theta, a12 = -sin theta
+        , a21 = sin theta, a22 = cos theta
         }
     )
 
@@ -84,10 +63,8 @@ scaleBy :: (Num a) => (a, a) -> (a, a) -> (a, a)
 scaleBy (x, y) =
   applyMatrixToPoint
     ( Matrix2x2
-        { a11 = x
-        , a12 = 0
-        , a21 = 0
-        , a22 = y
+        { a11 = x, a12 = 0
+        , a21 = 0, a22 = y
         }
     )
 
@@ -101,24 +78,18 @@ flipX :: (Num a) => (a, a) -> (a, a)
 flipX = scaleBy (-1, 1)
 
 flipY :: (Num a) => (a, a) -> (a, a)
-flipY =
+flipY = 
   scaleBy (1, -1)
 
 flipXY :: (Num a) => (a, a) -> (a, a)
 flipXY = scaleBy (-1, -1)
 
-translate :: (Num b) => (b, b) -> (b, b) -> (b, b)
+translate :: Num b => (b, b) -> (b, b) -> (b, b)
 translate (x, y) =
-  applyMatrix3x3ToPoint
-    ( Matrix3x3
-        { b11 = 1
-        , b12 = 0
-        , b13 = x
-        , b21 = 0
-        , b22 = 1
-        , b23 = y
-        , b31 = 0
-        , b32 = 0
-        , b33 = 1
+  applyMatrix3x3ToPoint 
+    ( Matrix3x3 
+        { b11 = 1, b12 = 0, b13 = x
+        , b21 = 0, b22 = 1, b23 = y
+        , b31 = 0, b32 = 0, b33 = 1
         }
     )
